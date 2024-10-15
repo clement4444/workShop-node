12e2ef87d667ded45e8973a525eb83d28a368fdf
clément4444 <clement.fiquet0@gmail.com>
2024-10-15 11:32:50 +0200

    bug réparer

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ import chalk from "chalk";
 console.log(`${chalk.cyanBright("clement")}
 ${chalk.red("philippe")}
 ${chalk.green("yavuz")}
-${chalk.yellow("alexandre")}`);
+${chalk.yellow("alexandre")}
+${chalk.blackBright("alexandre")}`);
